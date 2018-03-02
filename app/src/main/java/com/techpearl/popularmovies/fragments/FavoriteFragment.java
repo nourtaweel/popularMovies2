3b8915d030b3a6257c5080b298a614fe42660816
nour taweel <nour.taweel@gmail.com>
2018-03-02 11:40:27 +0000

    Add fragments classes

---
+++ app/src/main/java/com/techpearl/popularmovies/fragments/FavoriteFragment.java
@@ -0,0 +1,30 @@
+package com.techpearl.popularmovies.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.techpearl.popularmovies.R;
+
+/**
+ * A fragment to show user's favorite movies
+ */
+public class FavoriteFragment extends Fragment {
+
+    public FavoriteFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_favorite, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/techpearl/popularmovies/fragments/PopularFragment.java
@@ -0,0 +1,30 @@
+package com.techpearl.popularmovies.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.techpearl.popularmovies.R;
+
+/**
+ * A fragment to show popular movies
+ */
+public class PopularFragment extends Fragment {
+
+
+    public PopularFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_popular, container, false);
+    }
+
+}
