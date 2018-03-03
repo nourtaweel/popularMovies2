807161bd9fb052b8e84ee1ed9a75310e83a9320b
nour taweel <nour.taweel@gmail.com>
2018-03-03 17:54:37 +0000

    Use Loader to view favorites

--- app/src/main/java/com/techpearl/popularmovies/fragments/FavoriteFragment.java
+++ app/src/main/java/com/techpearl/popularmovies/fragments/FavoriteFragment.java
@@ -1,19 +1,10 @@
 package com.techpearl.popularmovies.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.techpearl.popularmovies.DetailsActivity;
-import com.techpearl.popularmovies.R;
-import com.techpearl.popularmovies.adapters.MoviesAdapter;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import com.techpearl.popularmovies.model.Movie;
 import com.techpearl.popularmovies.utils.DataUtils;
 
@@ -22,15 +13,55 @@ import java.util.List;
 /**
  * A fragment to show user's favorite movies
  */
-public class FavoriteFragment extends BaseMoviesFragment {
+public class FavoriteFragment extends BaseMoviesFragment implements
+        LoaderManager.LoaderCallbacks<List<Movie>>{
     private static final String TAG = FavoriteFragment.class.getSimpleName();
+    private static final int FAVORITES_LOADER_ID = 1;
 
     public FavoriteFragment() {
     }
 
     @Override
     void loadMovieList() {
-        List<Movie> faves = DataUtils.getFavorites(getContext());
-        showResponse(faves);
+//        List<Movie> faves = DataUtils.getFavorites(getContext());
+//        showResponse(faves);
+        getLoaderManager().initLoader(FAVORITES_LOADER_ID, null, this);
     }
+
+    @Override
+    public Loader<List<Movie>> onCreateLoader(int i, Bundle bundle) {
+        return new AsyncTaskLoader<List<Movie>>(getContext()) {
+            List<Movie> mFavorites;
+            @Override
+            public List<Movie> loadInBackground() {
+                return DataUtils.getFavorites(getContext());
+            }
+
+            @Override
+            public void deliverResult(@Nullable List<Movie> data) {
+                mFavorites = data;
+                super.deliverResult(data);
+
+            }
+
+            @Override
+            protected void onStartLoading() {
+                if(mFavorites != null){
+                    deliverResult(mFavorites);
+                }else {
+                    forceLoad();
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Movie>> loader, List<Movie> movies) {
+        showResponse(movies);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Movie>> loader) {
+    }
+
 }
